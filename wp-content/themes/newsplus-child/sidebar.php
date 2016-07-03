4b6a2395edb68114e6b8e85a27ac93d859139c9c
Stephen <stephen.frx@gmail.com>
2016-07-03 17:02:06 +0800

    Add post list ellipsis in front page

--- wp-content/themes/newsplus-child/sidebar.php
+++ wp-content/themes/newsplus-child/sidebar.php
@@ -17,6 +17,7 @@ endif;?>
 $parent           = $categories[0]->category_parent;
 $parent_name      = get_cat_name($parent);
 ?>
+<aside class="widget widget_sub_categories">
 <h3 class="sb-title"><?php echo $parent_name ?></h3>
 <ul>
 <?php
@@ -29,6 +30,7 @@ foreach ($subcategories as $subcategory):
 </li>
 <?php endforeach;?>
 </ul>
+</aside>
 <?php if (is_page() || is_single()) {
 	if (is_active_sidebar($sb_usage)) {
 		dynamic_sidebar($sb_usage);
